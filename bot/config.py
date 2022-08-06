5a62809544f1b8dffaa178942163ebff193e3691
CineHub4U1 <106883661+CineHub4U1@users.noreply.github.com>
2022-08-06 11:27:59 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ try:
     API_HASH = config("API_HASH", "5c9541eefe8452186e9649e2effc1f3f")
     BOT_TOKEN = config("BOT_TOKEN", "5471076772:AAHBaC_ALIO1lQRCPoT9PzLsw4hx7tj-I50")
     DEV = 5211097098
-    OWNER = config("OWNER", "5471076772:AAHBaC_ALIO1lQRCPoT9PzLsw4hx7tj-I50")
+    OWNER = config("OWNER", "1164918935")
     ffmpegcode = ["-preset veryfast -c:v libx265 -s 1280:-2 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By DaddyCooL' -pix_fmt yuv420p -crf 24 -c:a libopus -b:a 128k -vbr on -c:s copy -map 0 threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/ebde50b591e0e6c19366c.png")
 except Exception as e:
