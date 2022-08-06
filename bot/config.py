79a1a397977a7a95491d207b340fcf1ace05fb64
CineHub4U1 <106883661+CineHub4U1@users.noreply.github.com>
2022-08-06 11:01:00 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ from decouple import config
 try:
     APP_ID = config("APP_ID", 7405235 cast=int)
     API_HASH = config("API_HASH", "5c9541eefe8452186e9649e2effc1f3f")
-    BOT_TOKEN = config("BOT_TOKEN", " 5471076772:AAHBaC_ALIO1lQRCPoT9PzLsw4hx7tj-I50")
+    BOT_TOKEN = config("BOT_TOKEN", "5471076772:AAHBaC_ALIO1lQRCPoT9PzLsw4hx7tj-I50")
     DEV = 5211097098
     OWNER = config("OWNER", "5471076772:AAHBaC_ALIO1lQRCPoT9PzLsw4hx7tj-I50")
     ffmpegcode = ["-preset veryfast -c:v libx265 -s 1280:-2 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By DaddyCooL' -pix_fmt yuv420p -crf 24 -c:a libopus -b:a 128k -vbr on -c:s copy -map 0 threads 1"]
