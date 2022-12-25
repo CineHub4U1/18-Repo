913bcc04bf803243c9d6286324755ad591fe1341
CineHub4U1 <106883661+CineHub4U1@users.noreply.github.com>
2022-12-25 18:35:24 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,11 +16,11 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("APP_ID", "20288183", cast=int)
+    API_HASH = config("API_HASH", "c552d415c8c88ac267891e1d01deaa57")
+    BOT_TOKEN = config("BOT_TOKEN" "5929336845:AAEot0q2AHflpowNzYBO6L1aZ2Gb_Esc_zA")
     DEV = 1664850827
-    OWNER = config("OWNER")
+    OWNER = config("OWNER", "1164918935")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMB = config("THUMBNAIL")
 except Exception as e:
