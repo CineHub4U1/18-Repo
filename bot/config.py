a9ce208fdfa4c4ef485aac8e91a7f2c2216dc240
CineHub4U1 <106883661+CineHub4U1@users.noreply.github.com>
2022-12-25 18:53:43 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -22,7 +22,7 @@ try:
     DEV = 1664850827
     OWNER = config("OWNER", "1164918935")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("THUMBNAIL")
+    THUMB = config("THUMBNAIL", "https://telegra.ph/file/cc878234da5eaddafc37f.jpg")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
